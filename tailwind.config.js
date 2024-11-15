30fbb9dcea29d83601ce20c179034a7ead418c03
Adrian Kucyk <kucykadrian2004@gmail.com>
2024-11-15 23:37:41 +0100

    fix: update background image configuration in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,11 @@ export default {
       screens: {
         extraSm: '425px',
       },
-      backgroundImage: (theme) => ({
+      backgroundImage: {
         contactFormGradient:
-          'linear-gradient(90deg, #f3f3f300, #f3f3f3a6), url(' +
-          theme('assets.FormIllustration') +
-          ')',
-        contactFormPure: 'url(' + theme('assets.FormIllustration') + ')',
-      }),
+          'linear-gradient(90deg, #f3f3f300, #f3f3f3a6), url(FormIllustration.svg)',
+        contactFormPure: 'url(FormIllustration.svg)',
+      },
       boxShadow: {
         BottomShadow: '0 5px 0 0 #191A23',
       },
